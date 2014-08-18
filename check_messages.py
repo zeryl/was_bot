cf658458bc124e098049c8516dfa715d1f61230a
Zeryl (Matt Carlson) <lordsaryon@gmail.com>
2014-08-18 12:56:39 -0500

    More changes, including auto-reply, better formatting, and marking as read

--- check_messages.py
+++ check_messages.py
@@ -15,11 +15,15 @@ for message in r.get_unread(limit=None):
     if (not message.was_comment):
         if (regex_signup.search(message.subject)):
             message.body = message.body.encode("ascii", "xmlcharrefreplace").decode("ascii", "xmlcharredreplace")
-            print ("Message from %s: Subject: %s: Body: %s" % (message.author.name, message.subject, message.body))
-            qslmethod = ""
-            bands = ""
-            callsign = ""
-            print ("%s | %s | /u/%s | %s | %s" % ("MO", callsign, message.author.name, qslmethod, bands))
+#            print ("Message from %s: Subject: %s: Body: %s" % (message.author.name, message.subject, message.body))
+            s = message.body.split('|')
+            qslmethod = s[2].strip()
+            bands = s[3].strip()
+            callsign = s[1].strip().upper()
+            state = s[0].strip().upper()
+            print ("%s | %s | /u/%s | %s | %s" % (state, callsign, message.author.name, qslmethod, bands))
+            message.mark_as_read()
+            message.reply("Thank you for your submission, it should be on the site soon!")
         else:
             print ("No signup command found %s" % (message.subject))
     else:
